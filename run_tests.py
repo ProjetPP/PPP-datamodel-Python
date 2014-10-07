ae9f2ebfad3ab57f5121c7a711eb3fc7809d49a6
Valentin Lorentz <progval@progval.net>
2014-10-07 11:45:26 +0200

    Add scrutinizer coverage test.

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 import unittest
 
-testsuite = unittest.TestLoader().discover('tests/')
-unittest.TextTestRunner(verbosity=1).run(testsuite)
+def main(): # pragma: no cover
+    testsuite = unittest.TestLoader().discover('tests/')
+    results = unittest.TextTestRunner(verbosity=1).run(testsuite)
+    if results.errors or results.failures:
+        exit(1)
+    else:
+        exit(0)
+
+if __name__ == '__main__':
+    main()
