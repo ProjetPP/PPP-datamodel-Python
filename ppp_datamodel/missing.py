62a956465abf5b9c0ed685a4545b3e46229b674d
Valentin Lorentz <progval@progval.net>
2014-10-10 16:28:44 +0200

    Use slots for triple/resource/missing.

--- ppp_datamodel/missing.py
+++ ppp_datamodel/missing.py
@@ -7,6 +7,7 @@ class Missing(AbstractNode):
     """Represents a “missing” node.
     https://github.com/ProjetPP/Documentation/blob/master/data-model.md#missing
     """
+    __slots__ = ()
     _type = 'missing'
     _possible_attributes = ()
 
