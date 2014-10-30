ec1f2195ada37c670334e761ed3d8735e6b761fb
Valentin Lorentz <progval@progval.net>
2014-10-30 16:55:17 +0100

    Bump version number to 0.5.1 (previous release failed).

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ppp_datamodel',
-    version='0.5',
+    version='0.5.1',
     description='Data model for the Projet Pensées Profondes.',
     url='https://github.com/ProjetPP/PPP-datamodel-Python',
     author='Valentin Lorentz',
