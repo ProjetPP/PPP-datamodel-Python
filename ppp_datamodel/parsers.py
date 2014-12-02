343146ae1c97abad5ea628278b7a54867a33c682
Valentin Lorentz <progval@progval.net>
2014-12-02 21:49:15 +0100

    Add support for more operators.

--- ppp_datamodel/parsers.py
+++ ppp_datamodel/parsers.py
@@ -2,14 +2,14 @@
 
 import collections
 
-from .nodes import List, Triple, Missing, Resource
+from .nodes import List, Triple, Missing, Resource, Or, And, Intersection, Union
 
 __all__ = ['ParseError', 'parse_triples']
 
 class ParseError(Exception):
     pass
 
-OPERATORS = ('[', ']', '(', ')', ',')
+OPERATORS = ('[', ']', '(', ')', ',', '/\\', r'\\/')
 def lex_triples(sentence):
     buff = ''
     for char in sentence:
@@ -23,11 +23,13 @@ def lex_triples(sentence):
                 yield last_token.strip()
             buff = ''
             yield op
+    if buff.strip():
+        yield buff.strip()
 
 stackitem = collections.namedtuple('stackitem', 'type tree')
 TYPES = {
-        '(': 'triple', ')': 'triple',
-        '[': 'list', ']': 'list',
+        '(': 'paren', ')': 'paren',
+        '[': 'brack', ']': 'brack',
         }
 OPEN = '(['
 CLOSE = ')]'
@@ -47,24 +49,54 @@ def parse_forest(tokens):
             stack.pop(-1)
         elif token == ',':
             pass
+        elif token in ('\\/', '/\\'):
+            stack[-1].tree.append(stackitem('operator', token))
         else:
             stack[-1].tree.append(stackitem('resource', token))
     if stack[-1].type != 'root':
         raise ParseError('Stack is not empty')
     return forest
+
+
+OPERATOR_TO_CLASS = {
+        '\\/': Or, '∨': Or,
+        '/\\': And, '∧': And,
+        '∪': Union,
+        '∩': Intersection,
+        }
+def make_operators(lists):
+    operators = lists[1::][::2] # Odd
+    items = lists[::2] # Even
+    if len(lists) % 2 != 1 or \
+            any(x.tree not in OPERATOR_TO_CLASS for x in operators):
+        raise ParseError('Juxtaposition of items.')
+    op = operators[0]
+    if any(x != op for x in operators):
+        raise ParseError('All operators at same level should be the same.')
+    cls = OPERATOR_TO_CLASS[op.tree]
+    return cls(list(map(make_triples, items)))
 def make_triples(lists):
-    assert isinstance(lists, stackitem), lists
-    if lists.type == 'resource':
+    if not isinstance(lists, stackitem):
+        assert isinstance(lists, list), lists
+        if len(lists) == 1:
+            # This is “priority parenthesing”
+            return make_triples(lists[0])
+        else:
+            return make_operators(lists)
+    elif lists.type == 'resource':
         if lists.tree == '?':
             return Missing()
         else:
             return Resource(value=lists.tree)
-    elif lists.type == 'triple':
-        if len(lists.tree) == 3:
+    elif lists.type == 'paren':
+        if len(lists.tree) == 1:
+            return make_triples(lists.tree[0])
+        elif len(lists.tree) == 3 and \
+                all(x.type != 'operator' for x in lists.tree):
             return Triple(*map(make_triples, lists.tree))
         else:
-            raise ParseError('“Triple” of size %d: %r' % (len(lists.tree), lists))
-    elif lists.type == 'list':
+            return make_triples(lists.tree)
+    elif lists.type == 'brack':
         return List(list(map(make_triples, lists.tree)))
     else:
         assert False, lists
@@ -72,5 +104,5 @@ def make_triples(lists):
 
 
 def parse_triples(sentence):
-    return list(map(make_triples, parse_forest(lex_triples(sentence))))
+    return make_triples(parse_forest(lex_triples(sentence)))
 
--- tests/test_triple_parser.py
+++ tests/test_triple_parser.py
@@ -1,11 +1,11 @@
 from unittest import TestCase
 
-from ppp_datamodel import Triple, Resource, Missing, List
+from ppp_datamodel import Triple, Resource, Missing, List, Or, And
 from ppp_datamodel.parsers import parse_triples
 
 class TripleParserTestCase(TestCase):
     def testBasics(self):
-        self.assertEqual(parse_triples('(foo, (bar, ?, (?, qux, quux)), ?)'), [
+        self.assertEqual(parse_triples('(foo, (bar, ?, (?, qux, quux)), ?)'),
                 Triple(
                     Resource('foo'),
                     Triple(
@@ -18,18 +18,18 @@ class TripleParserTestCase(TestCase):
                             ),
                         ),
                     Missing()
-                )])
+                ))
     def testList(self):
-        self.assertEqual(parse_triples('[]'), [List([])])
-        self.assertEqual(parse_triples('[foo]'), [List([Resource('foo')])])
-        self.assertEqual(parse_triples('[(?,?,?)]'), [List([
+        self.assertEqual(parse_triples('[]'), List([]))
+        self.assertEqual(parse_triples('[foo]'), List([Resource('foo')]))
+        self.assertEqual(parse_triples('[(?,?,?)]'), List([
                 Triple(
                     Missing(),
                     Missing(),
                     Missing(),
                     )
-                ])])
-        self.assertEqual(parse_triples('(foo, [bar, (?, ?, baz)], (?, bar, [qux]))'), [
+                ]))
+        self.assertEqual(parse_triples('(foo, [bar, (?, ?, baz)], (?, bar, [qux]))'),
                 Triple(
                     Resource('foo'),
                     List([
@@ -47,5 +47,27 @@ class TripleParserTestCase(TestCase):
                             Resource('qux')
                         ]),
                     ),
-                )])
+                ))
+
+    def testOperators(self):
+        t1 = And([
+                Resource('foo'),
+                Resource('bar'),
+                ])
+        self.assertEqual(parse_triples('foo /\ bar'), t1)
+        self.assertEqual(parse_triples('(foo /\ bar)'), t1)
+        t2 = Or([
+                Triple(
+                    Resource('foo'),
+                    Missing(),
+                    Missing()
+                    ),
+                Triple(
+                    Missing(),
+                    Resource('bar'),
+                    Missing()
+                    )
+                ])
+        self.assertEqual(parse_triples('(foo, ?, ?) \/ (?, bar, ?)'), t2)
+        self.assertEqual(parse_triples('((foo, ?, ?) \/ (?, bar, ?))'), t2)
 
