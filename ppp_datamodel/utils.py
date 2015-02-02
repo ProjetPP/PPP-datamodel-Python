3d8d60f312b09e7a9744f70153866d195e795f1b
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2015-02-02 09:07:31 +0100

    Add isincluded function.

--- ppp_datamodel/utils.py
+++ ppp_datamodel/utils.py
@@ -1,6 +1,6 @@
 """Utilities for using the PPP datamodel."""
 
-from . import Missing
+from . import Resource, Triple, Missing, Intersection, List, Union, And, Or, Exist, First, Last, Sort
 
 def contains_missing(tree):
     def predicate(node, childs):
@@ -9,3 +9,23 @@ def contains_missing(tree):
         else:
             return any(childs.values())
     return tree.fold(predicate)
+
+def isincluded(tree1,tree2):
+    """
+        Return True if and only if tree1 is included in tree2.
+    """
+    if isinstance(tree1,Resource):
+        tree1=List([tree1])
+    if isinstance(tree2,Resource):
+        tree2=List([tree2])
+    if type(tree1) != type(tree2):
+        return False
+    if isinstance(tree1,List):
+        return set(tree1.list).issubset(set(tree2.list))
+    if isinstance(tree1,Triple):
+        return  isincluded(tree1.subject,tree2.subject) and\
+            isincluded(tree1.predicate,tree2.predicate) and\
+            isincluded(tree1.object,tree2.object)
+    if isinstance(tree1,Sort):
+        return tree1.predicate == tree2.predicate and isincluded(tree1.list,tree2.list)
+    return isincluded(tree1.list,tree2.list)
