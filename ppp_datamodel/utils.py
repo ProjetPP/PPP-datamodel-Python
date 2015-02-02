671bfdd211dd05a30722aaae436dfaa8c966bbbd
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2015-02-02 22:27:09 +0100

    Method assertIncluded.

--- ppp_datamodel/utils.py
+++ ppp_datamodel/utils.py
@@ -1,6 +1,7 @@
 """Utilities for using the PPP datamodel."""
 
 from . import Resource, Triple, Missing, Intersection, List, Union, And, Or, Exists, First, Last, Sort
+import unittest
 
 def contains_missing(tree):
     def predicate(node, childs):
@@ -10,34 +11,61 @@ def contains_missing(tree):
             return any(childs.values())
     return tree.fold(predicate)
 
-def isincluded(tree1, tree2):
-    """
-        Return True if and only if tree1 is included in tree2.
-    """
-    if isinstance(tree1,Resource):
-        tree1=List([tree1])
-    if isinstance(tree2,Resource):
-        tree2=List([tree2])
-    if type(tree1) != type(tree2):
-        return False
-    elif isinstance(tree1, Missing):
-        return True
-    elif isinstance(tree1, List):
-        return set(tree1.list).issubset(set(tree2.list))
-    elif isinstance(tree1, Triple):
-        return  isincluded(tree1.subject, tree2.subject) and\
-            isincluded(tree1.predicate, tree2.predicate) and\
-            isincluded(tree1.object, tree2.object)
-    elif isinstance(tree1,Sort):
-        return tree1.predicate == tree2.predicate and isincluded(tree1.list, tree2.list)
-    elif isinstance(tree1,(First, Last, Exists)):
-        return isincluded(tree1.list, tree2.list)
-    elif isinstance(tree1,(Intersection, Union, And, Or)):
-        if len(tree1.list) != len(tree2.list):
-            return False
-        for elt in tree1.list:
-            if not any(isincluded(elt,x) for x in tree2.list):
-                return False
-        return True
-    else:
-        raise Exception('Unknown class.')
+class InclusionAssertion:
+
+    def isincluded(self,tree1, tree2, originalTree1, originalTree2):
+        """
+            Check wether tree1 is included in tree2.
+            originalTree1 and originalTree2 are the trees given at the call of assertIncluded.
+        """
+        if isinstance(tree1,Resource):
+            tree1=List([tree1])
+        if isinstance(tree2,Resource):
+            tree2=List([tree2])
+        if type(tree1) != type(tree2):
+            raise AssertionError('Different types for %s and %s.\n%s ⊈ %s.' % (tree1, tree2, originalTree1, originalTree2))
+        elif isinstance(tree1, Missing):
+            return
+        elif isinstance(tree1, List):
+            if not set(tree1.list).issubset(set(tree2.list)):
+                raise AssertionError('List %s not included in List %s.\n%s ⊈ %s.' % (tree1, tree2, originalTree1, originalTree2))
+        elif isinstance(tree1, Triple):
+            return  self.isincluded(tree1.subject, tree2.subject, originalTree1, originalTree2) and\
+                self.isincluded(tree1.predicate, tree2.predicate, originalTree1, originalTree2) and\
+                self.isincluded(tree1.object, tree2.object)
+        elif isinstance(tree1,Sort):
+            if tree1.predicate != tree2.predicate:
+                raise AssertionError('Different predicate: %s and %s.\n%s ⊈ %s.' % (tree1.predicate, tree2.predicate, originalTree1, originalTree2))
+            else:
+                return self.isincluded(tree1.list, tree2.list, originalTree1, originalTree2)
+        elif isinstance(tree1,(First, Last, Exists)):
+            return self.isincluded(tree1.list, tree2.list, originalTree1, originalTree2)
+        elif isinstance(tree1,(Intersection, Union, And, Or)):
+            if len(tree1.list) != len(tree2.list):
+                raise AssertionError('Different list length for %s and %s.\n%s ⊈ %s.' % (tree1, tree2, originalTree1, originalTree2))
+            for elt in tree1.list:
+                if not self.contains(elt,tree2.list):
+                    raise AssertionError('%s does not belong to %s.\n%s ⊈ %s.' % (elt, tree2.list, originalTree1, originalTree2))
+            return
+        else:
+            raise Exception('Unknown class.')
+
+    def contains(self,elt,l):
+        """
+            Return True if and only if l contains elt (in the isincluded meaning).
+        """
+        included = False
+        for elt2 in l:
+            try:
+                self.isincluded(elt,elt2,None,None)
+            except AssertionError:
+                continue
+            included = True
+            break
+        return included
+
+    def assertIncluded(self,tree1,tree2):
+        """
+            Check wether tree1 is included in tree2.
+        """
+        return self.isincluded(tree1,tree2,tree1,tree2)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -15,51 +15,64 @@ class UtilsTests(TestCase):
         self.assertFalse(f(T(subject=R(value='foo'), object=T(subject=R()))))
         self.assertTrue(f(M()))
     def testInclusionBasic(self):
-        f = utils.isincluded
-        self.assertTrue(f(Missing(), Missing()))
-        self.assertTrue(f(Resource('foo'), Resource('foo')))
-        self.assertFalse(f(Resource('foo'), Resource('bar')))
-        self.assertTrue(f(List([Resource('foo')]), Resource('foo')))
-        self.assertFalse(f(List([Resource('foo')]), Resource('bar')))
-        self.assertTrue(f(Resource('foo'), List([Resource('foo')])))
-        self.assertFalse(f(Resource('foo'), List([Resource('bar')])))
-        self.assertTrue(f(List([Resource('foo')]), List([Resource('foo')])))
-        self.assertFalse(f(List([Resource('foo')]), List([Resource('bar')])))
-        self.assertTrue(f(List([Resource('foo')]), List([Resource('foo'), Resource('bar')])))
-        self.assertFalse(f(List([Resource('foo'), Resource('bar')]), List([Resource('foo')])))
+        f = utils.InclusionAssertion().assertIncluded
+        f(Missing(), Missing())
+        f(Resource('foo'), Resource('foo'))
+        with self.assertRaises(AssertionError):
+            f(Resource('foo'), Resource('bar'))
+        f(List([Resource('foo')]), Resource('foo'))
+        with self.assertRaises(AssertionError):
+            f(List([Resource('foo')]), Resource('bar'))
+        f(Resource('foo'), List([Resource('foo')]))
+        with self.assertRaises(AssertionError):
+            f(Resource('foo'), List([Resource('bar')]))
+        f(List([Resource('foo')]), List([Resource('foo')]))
+        with self.assertRaises(AssertionError):
+            f(List([Resource('foo')]), List([Resource('bar')]))
+        f(List([Resource('foo')]), List([Resource('foo'), Resource('bar')]))
+        with self.assertRaises(AssertionError):
+            f(List([Resource('foo'), Resource('bar')]), List([Resource('foo')]))
     def testInclusionDifferentType(self):
-        f = utils.isincluded
+        f = utils.InclusionAssertion().assertIncluded
         l = [Resource('foo'), Missing(), Triple(Resource('foo'), Resource('foo'), Resource('foo')),\
             Intersection([Resource('foo')]), Union([Resource('foo')]),\
             And([Resource('foo')]), Or([Resource('foo')]), Exists(Resource('foo')),\
             First(Resource('foo')), Last(Resource('foo')), Sort(Resource('foo'), Resource('pred'))]
         for (t1, t2) in itertools.permutations(l, 2):
-            self.assertFalse(f(t1, t2))
+            with self.assertRaises(AssertionError):
+                self.assertFalse(f(t1, t2))
     def testInclusionTriple(self):
-        f = utils.isincluded
+        f = utils.InclusionAssertion().assertIncluded
         tree1=Triple(Resource('foo'), List([Resource('a'), Resource('b')]), Missing())
         tree2=Triple(List([Resource('bar'), Resource('foo')]), List([Resource('a'), Resource('d'), Resource('b')]), Missing())
-        self.assertTrue(f(tree1, tree2))
-        self.assertFalse(f(tree2, tree1))
+        f(tree1, tree2)
+        with self.assertRaises(AssertionError):
+            f(tree2, tree1)
     def testInclusionFirstLastSort(self):
-        f = utils.isincluded
+        f = utils.InclusionAssertion().assertIncluded
         tree1=Triple(Resource('foo'), List([Resource('a'), Resource('b')]), Missing())
         tree2=Triple(List([Resource('bar'), Resource('foo')]), List([Resource('a'), Resource('d'), Resource('b')]), Missing())
         for op in [Last,First]:
-            self.assertTrue(f(op(tree1), op(tree2)))
-            self.assertFalse(f(op(tree2), op(tree1)))
-        self.assertTrue(f(Sort(tree1, Resource('pred')), Sort(tree2, Resource('pred'))))
-        self.assertFalse(f(Sort(tree2, Resource('pred')), Sort(tree1, Resource('pred'))))
-        self.assertFalse(f(Sort(tree1, Resource('pred')), Sort(tree2, Resource('derp'))))
+            f(op(tree1), op(tree2))
+            with self.assertRaises(AssertionError):
+                f(op(tree2), op(tree1))
+        f(Sort(tree1, Resource('pred')), Sort(tree2, Resource('pred')))
+        with self.assertRaises(AssertionError):
+            f(Sort(tree2, Resource('pred')), Sort(tree1, Resource('pred')))
+        with self.assertRaises(AssertionError):
+            f(Sort(tree1, Resource('pred')), Sort(tree2, Resource('derp')))
     def testInclusionIntersectionUnionAndOr(self):
-        f = utils.isincluded
+        f = utils.InclusionAssertion().assertIncluded
         tree1=Triple(Resource('foo'), List([Resource('a'), Resource('b')]), Missing())
         tree2=Triple(List([Resource('bar'), Resource('foo')]), List([Resource('a'), Resource('d'), Resource('b')]), Missing())
         tree3=Missing()
         for op in [Intersection,Union,And, Or]:
-            self.assertTrue(f(op([tree1]), op([tree2])))
-            self.assertFalse(f(op([tree2]), op([tree1])))
-            self.assertTrue(f(op([tree1, tree3]), op([tree1, tree3])))
-            self.assertTrue(f(op([tree1, tree3]), op([tree3, tree1])))
-            self.assertFalse(f(op([tree1, tree3]), op([tree1])))
-            self.assertFalse(f(op([tree1]), op([tree3])))
+            f(op([tree1]), op([tree2]))
+            with self.assertRaises(AssertionError):
+                f(op([tree2]), op([tree1]))
+            f(op([tree1, tree3]), op([tree1, tree3]))
+            f(op([tree1, tree3]), op([tree3, tree1]))
+            with self.assertRaises(AssertionError):
+                f(op([tree1, tree3]), op([tree1]))
+            with self.assertRaises(AssertionError):
+                f(op([tree1]), op([tree3]))
