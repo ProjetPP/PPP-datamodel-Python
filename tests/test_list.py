f523ac7b25358ddd9f3faca42bb85c264a1b9b11
Valentin Lorentz <valentin.lorentz@ens-lyon.fr>
2015-05-15 22:54:17 +0200

    Add equality between a singleton list and its element. Closes GH-45.

---
+++ tests/test_list.py
@@ -0,0 +1,15 @@
+from ppp_datamodel import AbstractNode, Resource, List, Triple, Missing
+from ppp_datamodel import Union, first, Nth, Exists
+
+from unittest import TestCase
+
+R = Resource
+
+class ListTests(TestCase):
+    def testEquality(self):
+        self.assertEqual(List([R('foo'), R('bar')]), List([R('foo'), R('bar')]))
+        self.assertNotEqual(List([R('foo'), R('bar')]), List([R('foo'), R('baz')]))
+        self.assertEqual(List([R('foo')]), List([R('foo')]))
+        self.assertEqual(List([R('foo')]), R('foo'))
+        self.assertNotEqual(List([R('foo')]), R('bar'))
+
